52efe2e043027f843af1b37fcf43020c27196db5
aravindh203 <aravindhkumar218@gmail.com>
2023-01-09 17:47:14 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,51 @@
+             /*1*/
+spread1=(...a)=>console.log(a);
+spread1(1,2,3);
+
+
+             /*2*/
+spread2=(...a)=>console.log(a);
+spread2(4,5,6);
+
+
+             /*3*/
+spread3=(...a)=>console.log(a);
+spread3(7,8,9);
+
+
+             /*4*/
+spread4=(...a)=>console.log(a);
+spread4("a","b","c");
+
+
+             /*5*/
+spread5=(...a)=>console.log(a);
+spread5("a","b",5);
+
+
+             /*6*/
+spread6=(...a)=>console.log(a);
+spread6({name:"aravindh",age:20},9);
+
+
+             /*7*/
+spread7=(...a)=>console.log(a);
+spread7("a","b","c",{profession:"frontenddeveloper",qualification:"degree"});
+
+
+             /*8*/
+spread8=(...a)=>console.log(a);
+spread8({name:"aravindh",age:20},{profession:"frontenddeveloper",qualification:"degree"});
+
+
+             /*9*/
+spread9=(...a)=>console.log(a);
+spread9(7,[3,4,5],{name:"hello"});
+
+
+             /*10*/
+spread10=(...a)=>console.log(a);
+spread10(11,["a","b","c"],{name:"hello"});
+
+
+
